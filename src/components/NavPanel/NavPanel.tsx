332976ecec1d3a1ef9d5b1130fc26d8804dd5ca6
Evgeny Berg <berg.evg.1@gmail.com>
2023-03-27 21:56:39 +0400

    feat: auth message

--- src/components/NavPanel/NavPanel.tsx
+++ src/components/NavPanel/NavPanel.tsx
@@ -9,28 +9,36 @@ import { ReactComponent as LikedSongIcon } from '../../assets/likedSongs.svg';
 
 const NavPanel: React.FC = () => {
     return (
-        <nav className='w-60 bg-black flex flex-col justify-between'>
+        <nav className='w-60 bg-black flex flex-col'>
             <div>
                 <Link to='/' className='pt-6 px-6 mb-[1.375rem] block'>
                     <SpotifyLogo fill='#fff' className='max-w-[8.125rem]' />
                 </Link>
+            </div>
 
+            <div>
                 <NavPanelLink to='/' iconName='home'>Home</NavPanelLink>
                 <NavPanelLink to='/search' iconName='search' >Search</NavPanelLink>
                 <NavPanelLink to='/collection' iconName='list' >Your Library</NavPanelLink>
+            </div>
 
+            <div className='flex-1'>
                 <NavPanelButton
+                    modalHeading='Create playlist'
+                    modalMessage='Log in to create and share playlists.'
                     leftItem={<GoogleMaterialIcon iconName='add_box' className='flex items-center' FILL={1} size={1.8} />}
                     className='mt-6'
                 >Create playlist</NavPanelButton>
 
                 <NavPanelButton
+                    modalHeading='Enjoy your liked songs'
+                    modalMessage="Log in to see all the songs you&#8217;ve liked in one easy playlist."
                     leftItem={
-                        <div className='w-6 h-6 ml-0.5 group-hover: flex justify-center items-center liked-songs-icon'>
+                        <div className='w-6 h-6 ml-0.5 group-hover: flex justify-center items-center liked-songs-icon mr-1'>
                             <LikedSongIcon className='fill-[#d3d3d3] group-hover:fill-white transition-[fill] duration-300' />
                         </div>
                     }
-                >Create playlist</NavPanelButton>
+                >Liked songs</NavPanelButton>
             </div>
 
             <div className='text-[0.6875rem] text-[#b3b3b3] flex flex-wrap gap-x-4 px-6 my-8'>
@@ -41,8 +49,9 @@ const NavPanel: React.FC = () => {
                 <a className='h-[1.875rem] block whitespace-nowrap' href='https://www.spotify.com/legal/privacy-policy/#s3/'>About Ads</a>
 
                 <button
-                    className={'relative h-8 border border-solid border-[#878787] transition-[transform,_border-width,_border-color]'
-                        .concat(' text-white rounded-full pl-8 pr-4 text-sm leading-8 font-semibold')
+                    className={'relative h-8 border border-solid leading-8 font-semibold'
+                        .concat(' text-white rounded-full pl-8 pr-4 mt-8 text-sm')
+                        .concat(' transition-[transform,_border-width,_border-color]')
                         .concat(' hover:scale-105 hover:border-white hover:sborder-2')}
                 >
                     <GoogleMaterialIcon size={1.2} className='absolute top-1/2 -translate-y-1/2 flex items-center left-3' iconName='language' />
--- src/components/NavPanel/NavPanelButton.tsx
+++ src/components/NavPanel/NavPanelButton.tsx
@@ -1,20 +1,80 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 type NavPanelButtonProps = JSX.IntrinsicElements['button'] & {
     leftItem: React.ReactNode
+
+    modalHeading: string
+    modalMessage: string
 };
 
-const NavPanelButton: React.FC<NavPanelButtonProps> = ({ className, children, leftItem, ...buttonProps }) => {
+const NavPanelButton: React.FC<NavPanelButtonProps> = ({ className, children, leftItem, modalHeading, modalMessage, ...buttonProps }) => {
+    const [isAuthMessageVisible, setIsAuthMessageVisible] = useState(false);
+
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const hideAuthMessage = useCallback((e: MouseEvent) => {
+        if (containerRef.current && !e.composedPath().includes(containerRef.current)) {
+            setIsAuthMessageVisible(false);
+            document.removeEventListener('click', hideAuthMessage);
+        }
+    }, [containerRef]);
+
+    const handleShowMessage = () => setIsAuthMessageVisible(true);
+
+    const handleHideMessage = () => setIsAuthMessageVisible(false);
+
+    useEffect(() => {
+        if (isAuthMessageVisible) {
+            document.addEventListener('click', hideAuthMessage);
+        }
+
+        return () => {
+            document.removeEventListener('click', hideAuthMessage);
+        }
+    }, [isAuthMessageVisible]);
 
     return (
-        <button
-            className={'group flex items-center gap-4 h-10 w-full px-4 text-sm font-bold'
-                .concat(` ${className}` || '')}
-            {...buttonProps}
-        >
-            {leftItem}
-            <p className='text-[#d3d3d3] transition-[color] leading-4 group-hover:text-white duration-300'>{children}</p>
-        </button>
+        <div ref={containerRef} className='relative'>
+            <button
+                onClick={handleShowMessage}
+                className={'group flex items-center gap-4 h-10 w-full px-4 text-sm font-bold relative'
+                    .concat(` ${className}` || '')}
+                {...buttonProps}
+            >
+                {leftItem}
+                <p className='text-[#d3d3d3] transition-[color] leading-4 group-hover:text-white duration-300'>{children}</p>
+            </button>
+
+            <div
+                className={'absolute top-0 -right-5 h-40 w-[20.75rem] bg-[#0d72ea] p-4 rounded-lg'
+                    .concat(' flex flex-col justify-between transition-[transform,_opacity] select-none')
+                    .concat(' ', isAuthMessageVisible ? 'translate-x-[calc(100%_-_0.5rem)]' : 'opacity-0 translate-x-full')}
+            >
+                <div
+                    className={'absolute top-0 left-0 translate-y-3 -translate-x-full'
+                        .concat(' border-8 border-l-0 border-solid border-transparent border-r-[#0d72ea]')}
+                ></div>
+
+                <div>
+                    <h2 className='text-lg leading-none font-bold mb-2'>{modalHeading}</h2>
+                    <p className='text-sm'>{modalMessage}</p>
+                </div>
+
+                <div className='flex justify-end'>
+                    <button
+                        onClick={handleHideMessage}
+                        className={'w-[5.5rem] h-8 text-[#6a6a6a] text-sm whitespace-nowrap font-bold block'
+                            .concat(' transition-[color,_transform]')
+                            .concat(' hover:text-white hover:scale-105')}
+                    >Not now</button>
+                    <button
+                        className={'h-8 w-[4.5rem] bg-white text-black rounded-full text-sm'
+                            .concat(' transition-[color,_transform]')
+                            .concat(' hover:scale-105')}
+                    >Log in</button>
+                </div>
+            </div>
+        </div>
     )
 }
 export default NavPanelButton;
